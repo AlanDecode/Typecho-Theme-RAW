6f479dee2e9510a763e588a3bd6ab16c277496f0
AlanDecode <zytbuaa1415@gmail.com>
2018-10-07 17:33:40 +0800

    移除 Open Sans 字体

--- component/head.php
+++ component/head.php
@@ -58,7 +58,7 @@
     <link rel="stylesheet" href="<?php echo RAW::staticPath($this->options->CDNPath,'/hljs/styles/atom-one-dark.css','','css');?>">
     <link rel="stylesheet" href="<?php echo RAW::staticPath($this->options->CDNPath,'/owo/owo.min.css','','css');?>">
     <link rel="stylesheet" href="<?php echo RAW::staticPath($this->options->CDNPath,'/zoomjs/zoom.css','','css');?>">
-    <link rel="stylesheet" href="<?php echo RAW::staticPath($this->options->CDNPath,'/css/RAW.css','8D592522','css'); ?>">
+    <link rel="stylesheet" href="<?php echo RAW::staticPath($this->options->CDNPath,'/css/RAW.css','8D592523','css'); ?>">
     <?php endif; ?>
      
     <!-- JS -->
