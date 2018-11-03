b0e580734c8baa4a47ab30d3a6c192efbbe05b99
AlanDecode <zytbuaa1415@gmail.com>
2018-11-03 23:23:10 +0800

    修改后台设置项顺序

--- functions.php
+++ functions.php
@@ -34,20 +34,20 @@ function themeConfig($form) {
     //基本设置
     $sitelogo=new Typecho_Widget_Helper_Form_Element_Textarea('sitelogo', NULL, NULL, _t('左上角站点文字'), _t('左上角站点标题，不填写默认为系统站点标题。'));
     $form->addInput($sitelogo);
-    $footerinfo=new Typecho_Widget_Helper_Form_Element_Textarea('footerinfo', NULL, NULL, _t('页面底部输出内容'), _t('你可以输入需要在页面底部输出的内容，包括备案号等。请使用标准的 HTML 语法书写。'));
-    $form->addInput($footerinfo);
-    $headinfo=new Typecho_Widget_Helper_Form_Element_Textarea('headinfo', NULL, NULL, _t('head 标签内输出信息'), _t('这里的内容会输出在 head 标签靠前的位置，不建议在这里加入 CSS，你可以输入一些 meta 标签等。'));
-    $form->addInput($headinfo);
-    $headernav=new Typecho_Widget_Helper_Form_Element_Textarea('headernav', NULL, NULL, _t('顶部导航链接'), _t('顶部导航链接，一行一个，格式：图标,名称,链接，例如：home,首页,/ 。图标名称参考 font-awesome。小屏幕会被隐藏。'));
-    $form->addInput($headernav);
     $left_link=new Typecho_Widget_Helper_Form_Element_Textarea('left_link', NULL, NULL, _t('侧边栏头像打开链接'), _t('输入点击侧边栏头像要打开的位置，默认为站点首页。'));
     $form->addInput($left_link);
+    $headernav=new Typecho_Widget_Helper_Form_Element_Textarea('headernav', NULL, NULL, _t('顶部导航链接'), _t('顶部导航链接，一行一个，格式：图标,名称,链接，例如：home,首页,/ 。图标名称参考 font-awesome。小屏幕会被隐藏。'));
+    $form->addInput($headernav);
     $aside_nav=new Typecho_Widget_Helper_Form_Element_Textarea('aside_nav', NULL, NULL, _t('侧边栏导航链接'), _t('请直接以 a 标签书写，小屏幕中点击底栏按钮可以显示。一行一个。'));
     $form->addInput($aside_nav);
     $aside_link=new Typecho_Widget_Helper_Form_Element_Textarea('aside_link', NULL, NULL, _t('全站友链'), _t('全站侧边栏友情链接，请直接以 a 标签书写。一行一个。'));
     $form->addInput($aside_link);
     $site_bg=new Typecho_Widget_Helper_Form_Element_Textarea('site_bg', NULL, NULL, _t('站点背景图'), _t('填写图像链接。'));
     $form->addInput($site_bg);
+    $footerinfo=new Typecho_Widget_Helper_Form_Element_Textarea('footerinfo', NULL, NULL, _t('页面底部输出内容'), _t('你可以输入需要在页面底部输出的内容，包括备案号等。请使用标准的 HTML 语法书写。'));
+    $form->addInput($footerinfo);
+    $headinfo=new Typecho_Widget_Helper_Form_Element_Textarea('headinfo', NULL, NULL, _t('head 标签内输出信息'), _t('这里的内容会输出在 head 标签靠前的位置，不建议在这里加入 CSS，你可以输入一些 meta 标签等。'));
+    $form->addInput($headinfo);
 	$customhead = new Typecho_Widget_Helper_Form_Element_Textarea('customhead', NULL, NULL, _t('head 标签结束前输出信息'), _t('将输出在 head 标签结束前，你可以输入统计代码或者 CSS 等，也可以使用 link 标签引入别的 CSS、JS。'));
     $form->addInput($customhead);
     $customfooter = new Typecho_Widget_Helper_Form_Element_Textarea('customfooter', NULL, NULL, _t('body 标签结束前输出信息'), _t('将输出在 body 标签结束前，你可以输入 JS 或者 CSS 等。'));
