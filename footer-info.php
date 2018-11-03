b831c4d4b61a2ce7ffe05570c45e71db89b21b2b
AlanDecode <zytbuaa1415@gmail.com>
2018-11-03 12:02:02 +0800

    修复当文章内容太少时主页排版错乱的问题

--- footer-info.php
+++ footer-info.php
@@ -1,5 +1,5 @@
 <div id="footer-info" style="font-size:0.9em;color:var(--text-color);background:none;text-align:center;line-height:1.5;margin-bottom:50px">
     <a href="https://blog.imalan.cn" target="_self">© <?php echo $this->options->title; ?></a><br>
-    <a href="https://github.com/AlanDecode/Typecho-Theme-RAW" target="_blank">Theme RAW</a> Made with <span class="heart"><i class="fa fa-heart"></i></span> by 熊猫小A<br>
-   <?php echo $this->options->footerinfo; ?>
+    <a href="https://blog.imalan.cn/archives/163/" target="_blank">Theme RAW</a> made with <span class="heart"><i class="fa fa-heart"></i></span> By <a href="https://imalan.cn">熊猫小A</a><br>
+    <?php echo $this->options->footerinfo; ?>
 </div>
