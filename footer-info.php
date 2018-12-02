128df5cbe1510893121a7feee93165437d5dc0c3
AlanDecode <zytbuaa1415@gmail.com>
2018-12-02 11:21:37 +0800

    简化页脚文字

--- footer-info.php
+++ footer-info.php
@@ -12,7 +12,6 @@
 
 <div id="footer-info" style="font-size:0.9em;color:var(--text-color);background:none;text-align:center;line-height:1.5;margin-bottom:50px">
     <a href="<?php echo $this->options->siteUrl; ?>" target="_self">© <?php echo $this->options->title; ?></a><br>
-    <a href="https://blog.imalan.cn/archives/163/" target="_blank">Theme RAW</a> made with <span class="heart"><i class="fa fa-heart"></i></span> By <a href="https://www.imalan.cn">熊猫小A</a><br>
-    <a href="http://typecho.org/" targte="_blank">Powered By Typecho</a><br>
+    <a href="https://blog.imalan.cn/archives/163/" target="_blank">Theme RAW</a> • <a href="http://typecho.org/" targte="_blank">Typecho</a><br>
     <?php echo $this->options->footerinfo; ?>
 </div>
