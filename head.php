35a30b83a07095b57a29e18015a64e54f0bd1bfa
AlanDecode <zytbuaa1415@gmail.com>
2018-12-02 22:26:24 +0800

    新的归档页样式

--- head.php
+++ head.php
@@ -38,8 +38,8 @@ Utils::exportHeader($this,$banner);
 <link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/owo/owo.min.css'); ?>" />
 <link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/hljs/styles/atom-one-light.css');?>">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/fancybox/jquery.fancybox.min.css');?>">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/main.css');?>">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/scheme-dark12.css');?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/main.3d.css');?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/scheme-dark13.css');?>">
 <?php if($this->options->columnorder=='1'):?>
 <style>
 aside{
