29beaeeb267a1841cf265c2c0609c5306d75e7b0
AlanDecode <zytbuaa1415@gmail.com>
2018-12-04 08:32:59 +0800

    '加载更多后点赞重载'

--- assets/main.23.js
+++ assets/main.23.js
@@ -49,6 +49,27 @@ function loadMorePosts(){
                 }
                 if(typeof(RAW)=="object"){
                     RAW.init();
+                    // 重新绑定文章点赞事件
+                    $(".post-like").click(function(){
+                        $(this).addClass("done");
+                    })
+                    $(".post-like").on("click", function(){
+                        var th = $(this);
+                        var id = th.attr('data-pid');
+                        var cookies = $.macaroon('_syan_like') || "";
+                        if (!id || !/^\d{1,10}$/.test(id)) return;
+                        if (-1 !== cookies.indexOf("," + id + ",")) return alert("您已经赞过了！");
+                        cookies ? cookies.length >= 160 ? (cookies = cookies.substring(0, cookies.length - 1), cookies = cookies.substr
+                (1).split(","), cookies.splice(0, 1), cookies.push(id), cookies = cookies.join(","), $.macaroon("_syan_like", "," + cookies + 
+                ",")) : $.macaroon("_syan_like", cookies + id + ",") : $.macaroon("_syan_like", "," + id + ",");
+                        $.post(likePath,{
+                        cid:id
+                        },function(data){
+                        th.addClass('actived');
+                        var zan = th.find('.like-num').text();
+                        th.find('.like-num').text(parseInt(zan) + 1);
+                        },'json');
+                    });
                     // 已点赞按钮高亮
                     var cookies = $.macaroon('_syan_like') || "";
                     $.each($(".post-like"),function(i,item){
