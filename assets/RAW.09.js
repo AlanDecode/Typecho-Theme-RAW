294e1f0a8dc567157e7b9618f15fe5d229402e42
AlanDecode <zytbuaa1415@gmail.com>
2018-12-16 10:34:35 +0800

    fix

--- assets/RAW.09.js
+++ assets/RAW.09.js
@@ -43,33 +43,35 @@ RAW={
                 maxHeight: '250px'
             });
         }
-        // 重新绑定文章点赞事件
-        $(".post-like").click(function(){
-            $(this).addClass("done");
-        })
-        $(".post-like").on("click", function(){
-            var th = $(this);
-            var id = th.attr('data-pid');
+        if(typeof($.macaroon)!="undefined"){
+            // 重新绑定文章点赞事件
+            $(".post-like").click(function(){
+                $(this).addClass("done");
+            })
+            $(".post-like").on("click", function(){
+                var th = $(this);
+                var id = th.attr('data-pid');
+                var cookies = $.macaroon('_syan_like') || "";
+                if (!id || !/^\d{1,10}$/.test(id)) return;
+                if (-1 !== cookies.indexOf("," + id + ",")) return alert("您已经赞过了！");
+                cookies ? cookies.length >= 160 ? (cookies = cookies.substring(0, cookies.length - 1), cookies = cookies.substr
+        (1).split(","), cookies.splice(0, 1), cookies.push(id), cookies = cookies.join(","), $.macaroon("_syan_like", "," + cookies + 
+        ",")) : $.macaroon("_syan_like", cookies + id + ",") : $.macaroon("_syan_like", "," + id + ",");
+                $.post(likePath,{
+                cid:id
+                },function(data){
+                th.addClass('actived');
+                var zan = th.find('.like-num').text();
+                th.find('.like-num').text(parseInt(zan) + 1);
+                },'json');
+            });
+            // 已点赞按钮高亮
             var cookies = $.macaroon('_syan_like') || "";
-            if (!id || !/^\d{1,10}$/.test(id)) return;
-            if (-1 !== cookies.indexOf("," + id + ",")) return alert("您已经赞过了！");
-            cookies ? cookies.length >= 160 ? (cookies = cookies.substring(0, cookies.length - 1), cookies = cookies.substr
-    (1).split(","), cookies.splice(0, 1), cookies.push(id), cookies = cookies.join(","), $.macaroon("_syan_like", "," + cookies + 
-    ",")) : $.macaroon("_syan_like", cookies + id + ",") : $.macaroon("_syan_like", "," + id + ",");
-            $.post(likePath,{
-            cid:id
-            },function(data){
-            th.addClass('actived');
-            var zan = th.find('.like-num').text();
-            th.find('.like-num').text(parseInt(zan) + 1);
-            },'json');
-        });
-        // 已点赞按钮高亮
-        var cookies = $.macaroon('_syan_like') || "";
-        $.each($(".post-like"),function(i,item){
-            var id = $(item).attr('data-pid');
-            if (-1 !== cookies.indexOf("," + id + ","))  $(item).addClass("done");
-        })
+            $.each($(".post-like"),function(i,item){
+                var id = $(item).attr('data-pid');
+                if (-1 !== cookies.indexOf("," + id + ","))  $(item).addClass("done");
+            })
+        }
         // 重载代码高亮
         $("pre code").each(function(i, block) {hljs.highlightBlock(block);});   
         // 重载 MathJax
